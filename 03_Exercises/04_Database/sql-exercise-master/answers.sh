4fe4701612fc2483a4645223cbefbfec2e6f3995
fawazshohiebar <fawazsho.132@gmail.com>
2022-12-09 14:48:33 +0200

    Basic Queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,9 @@
+SELECT name from students;
+select * from students  WHERE Age>30;
+select name from students WHERE  Gender="female" AND Age>30;
+select Points from students WHERE name="Alex"
+INSERT INTO students (name, Age,Gender,Points) VALUES ("fawaz",21, "male", 681);
+
+UPDATE students SET Points = 500 where Name='Basma'
+
+UPDATE students SET Points = 150 where Name='Alex'
