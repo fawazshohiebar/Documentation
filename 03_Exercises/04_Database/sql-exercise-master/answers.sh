eb82de0d5b58a4723c4d848c67098d8cb6af4ffb
fawazshohiebar <fawazsho.132@gmail.com>
2022-12-12 22:50:42 +0200

    sql commit

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -7,3 +7,66 @@ INSERT INTO students (name, Age,Gender,Points) VALUES ("fawaz",21, "male", 681);
 UPDATE students SET Points = 500 where Name='Basma'
 
 UPDATE students SET Points = 150 where Name='Alex'
+
+
+
+
+CREATE TABLE graduates (
+    ID int NOT NULL  ,
+    Name varchar(255) NOT NULL UNIQUE,
+    Age INT,
+    Gender varchar(255),
+    Points varchar(255),
+	Graduation varchar(255),
+	  PRIMARY KEY (ID)
+);
+
+
+INSERT INTO graduates VALUES (1,'Layal' , 18 , 'M',300, 08/09/2018) ; 
+
+
+DELETE  FROM students WHERE (name = 'layla');
+
+
+SELECT employees.Name , companies.Name
+From employees ,companies
+
+Where companies.name=employees.company ;
+
+
+
+
+
+
+SELECT employees.Name ,companies.Date
+From employees ,companies
+
+Where companies.name=employees.company  And companies.Date<2000;
+
+
+
+
+SELECT Company ,Role
+    FROM employees
+	WHERE Role="Graphic Designer";
+    
+
+
+
+
+	SELECT Name ,  max(Points) from students ;
+
+
+    SELECT avg(Points) from students;
+
+
+
+    SELECT count(Points) from students where Points=500;
+
+
+    SELECT name FROM students
+WHERE name LIKE '%s%';
+
+
+select * from students
+ORDER By Points DESC;
