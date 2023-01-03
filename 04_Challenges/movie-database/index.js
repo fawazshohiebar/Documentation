db2a7e5c2c8c4dedaedc03d44f50dfa97e9b8901
fawazshohiebar <fawazsho.132@gmail.com>
2023-01-03 19:32:18 +0200

    step3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,6 +1,20 @@
 const express =require ('express')
 const app =express()
-const port =3000
 
-app.all('*',(req,res)=>res.send('okkkk'))
-app.listen(port,()=>console.log('ok'))
+
+// app.all('*',(req,res)=>res.send('okkkk'))
+// app.listen(3002,()=>console.log('ok'))
+
+app.get('/',(req,res)=>{
+    res.send('ok')
+})
+app.get('/test',(req,res)=>{
+    res.send({status:200,message:"ok"})
+})
+
+app.get('/time',(req,res)=>{
+    const currenttime=new Date().toTimeString().slice(0, 5)
+    res.send({status:200,message:currenttime})
+})
+
+app.listen(3000,()=>console.log('example app listen on port 3000'))
