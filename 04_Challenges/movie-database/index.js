9fc08bc407380c0a51c53be941dd70306875b47b
fawazshohiebar <fawazsho.132@gmail.com>
2023-01-03 19:59:46 +0200

    step4

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -16,5 +16,20 @@ app.get('/time',(req,res)=>{
     const currenttime=new Date().toTimeString().slice(0, 5)
     res.send({status:200,message:currenttime})
 })
+app.get(`/hello/:id`,(req,res)=>{
+    const id =req.params.id || 'World'
+    res.json({status: 200, message: `Hello,${id}`})
+})
+app.get('/search',(req,res)=>{
+    const search=req.query.s
 
+if(search){
+res.send({status:200,message:"ok",data:search})
+}else{
+    res.send({status:500, error:true, message:"you have to provide a search"})
+
+}
+
+
+})
 app.listen(3000,()=>console.log('example app listen on port 3000'))
